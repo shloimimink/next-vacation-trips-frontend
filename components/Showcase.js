b1349aef4f1e41d84dc083f41426ed9aeea15d4d
shloimiminkowicz <shloimimink@gmail.com>
2021-07-28 18:24:37 -0500

    fixed

--- components/Showcase.js
+++ components/Showcase.js
@@ -3,8 +3,8 @@ import styles from '@/styles/Showcase.module.css'
 export default function Showcase() {
     return (
         <div className={styles.showcase}>
-            <h1>Welcome to the most unforgettable vacation places!!</h1>
-            <h2>find the most beautiful places to go!</h2>
+            <h1>Welcome to the most unforgettable vacation places!</h1>
+            <h2>Find the most beautiful places to go</h2>
         </div>
     )
 }
